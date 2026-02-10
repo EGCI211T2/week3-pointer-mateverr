b002e338ec1fde7c9f56c50c828e476261025ab6
mateverr <mattpittaya@gmail.com>
2026-02-10 11:02:24 +0000

    first draft with comments

--- main.cpp
+++ main.cpp
@@ -1,53 +1,67 @@
 #include <iostream>
 #include <iomanip>
-#define SIZE 10 // need to change into dynamic size
+
 
 using namespace std;
 
-int main(int argc,char *argv[])
+int main(int argc, char *argv[])
 {
 
-  int size = 0; // Initiate size at 0 waiting for user input
-  cout << "Enter size of array: ";
-  cin >> size;
-
   int *pa, *pb, i, temp;
-  int a[SIZE] ={1,2,3,4,5,6,7,8,9,10}; 
+
+  int n = argc - 1; // amount of data user entered
+
+  pa = new int[n]; // create array of n integers and store address in pointer pa 
+
+  int *a = pa;  // store starting address that pa points to
+
+  for (i = 0; i < n; i++)
+  {
+    *pa = atoi(argv[i+1]);
+    pa++;
+  }
+  // int a[SIZE] ={1,2,3,4,5,6,7,8,9,10}; 
   // change this to create array with size user input
 
-  pa = &a[0]; //pa =a;
+  // pa = &a[0]; //pa =a;
+  pa = a; // reset address stored in pa
+
   cout<<"Original: ";
 
-  for (i=0; i<SIZE-1; i++ ,pa++)
+  for (i=0; i<n-1; i++ ,pa++) // Loop through the array to output original 
   {
     cout<<setw(3)<<*pa ;//<<endl;
     //cout<<pa<<endl;
   }
 
-  cout<<setw(3)<<*pa<<endl;
+  cout<<setw(3)<<*pa<<endl; // to display last element
 
-  pa = &a[0];  
-  pb = &a[SIZE-1];
+  pa = a;  // reset address stored in pa
 
-  for (i=0; i<SIZE/2; i++)
+  pb = &a[n-1]; // assign address at the end of array
+
+  for (i=0; i<n/2; i++)
   {
-    temp = *pa;  
-    *pa = *pb;  
-    *pb = temp;
-    pa++;  pb--;
+    temp = *pa;  // store pa in temp
+    *pa = *pb;  // change val in pa to val in pb
+    *pb = temp; // now element in pb become element from pa
+    pa++;  // shift
+    pb--;
   }
 
-  pa=&a[0];
-  pb-=SIZE/2;
+  pa = a;
+  //pb-=SIZE/2;
+
   cout<<"Reversed: ";
   
-  for (i=0; i<SIZE-1; i++ ,pa++)
+  for (i=0; i<n-1; i++ ,pa++)
   {
     cout<<setw(3)<<*pa ;//<<endl;
     //cout<<pa<<endl;
   }
 
   cout<<setw(3)<<*pa<<endl;
+
 
   return 0;
 
